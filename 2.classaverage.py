4a8611075e8b7ff1338ddcfd249e236f3d08b164
Ancksunamun <88235353+Ancksunamun@users.noreply.github.com>
2021-08-04 18:12:49 +0430

    Add files via upload

---
+++ 2.classaverage.py
@@ -0,0 +1,17 @@
+print("enter the number of class member:")
+class_member_value= int(input("number of students:"))
+summ=0
+for class_member_value in range(class_member_value):
+
+
+    print(' enter the result one by one')
+
+    c=float(input ("enter the result:"))
+
+    summ=c+summ
+
+
+average= summ/(class_member_value+1)
+
+
+print (average)
